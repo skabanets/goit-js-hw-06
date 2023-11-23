1d22a0a206a7cd12e00c062318bb5ebdeb3f2b5d
Sergey Kabanets <skabanets97@gmail.com>
2023-11-23 11:17:04 +0200

    add task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
 
+const setForm = (e) => {
+  e.preventDefault();
+
+  const email = form.elements.email;
+  const password = form.elements.password;
+
+  if (email.value === "" || password.value === "") {
+    return alert("All fields must be completed!");
+  }
+
+  const formData = new Object();
+  formData.email = email.value;
+  formData.password = password.value;
+  console.log(formData);
+
+  form.reset();
+};
+
+form.addEventListener("submit", setForm);
