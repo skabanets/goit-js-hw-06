bf3de5643064333b1e4df3d2123bf78e5898907a
Sergey Kabanets <skabanets97@gmail.com>
2023-11-23 11:42:06 +0200

    add task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const changeColorBtn = document.querySelector(".change-color");
+const colorName = document.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColor = () => {
+  const backgroundColor = getRandomHexColor();
+  document.body.style.backgroundColor = `${backgroundColor}`;
+  colorName.textContent = `${backgroundColor}`;
+};
+
+changeColorBtn.addEventListener("click", changeColor);
